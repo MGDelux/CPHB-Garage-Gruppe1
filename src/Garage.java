a5064c98002e832becba6328a6352424e993bd62
Mathias <mathiastouboel@gmail.com>
2020-03-11 08:18:07 +0100

    hurtige ændringer basert på opgave beskrivelse

--- src/Garage.java
+++ src/Garage.java
@@ -16,4 +16,9 @@ public class Garage {
                 ", garageNavn='" + garageNavn + '\'' +
                 '}';
     }
-}
+    //Bil registerBil(int regNR,String maerke, String model, int aarGang, int antalDoore ){
+       // Bil(regNR,maerke,model,aarGang,antalDoore)
+
+    }
+
+
