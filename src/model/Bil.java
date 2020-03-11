26e0a503701576e37f596dd56fc598e628c2369e
Mathias <mathiastouboel@gmail.com>
2020-03-11 08:18:07 +0100

    hurtige ændringer basert på opgave beskrivelse

--- src/model/Bil.java
+++ src/model/Bil.java
@@ -6,9 +6,21 @@ public abstract class Bil {
     protected String model;
     protected int aargang;
     protected int antalDoore;
+
     protected double beregnGrønEjerAfgidt(double kmPrL){
         return 0.0;
     }
+
+    public Bil(int regNR,String maerke, String model, int aarGgang, int antalDoore) {
+        this.regNR = regNR;
+        this.maerke = maerke;
+        this.model = model;
+        this.aargang = aarGgang;
+        this.antalDoore = antalDoore;
+    }
 }
+
+
+
 
 
