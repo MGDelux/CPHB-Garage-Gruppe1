06625494ef2f83ffd390eb14758d83e48866b407
Tenz331 <61831296+Tenz331@users.noreply.github.com>
2020-03-11 08:43:46 +0100

    Getters og setters

--- src/model/BenzinBil.java
+++ src/model/BenzinBil.java
@@ -1,8 +1,17 @@
 package model;
 
 public class BenzinBil extends Bil {
-    int oktanTal;
-    double kmPrL;
+   private int oktanTal;
+   private int kmPrL;
+
+    public void setoktanTal() {
+        this.oktanTal = oktanTal;
+    }
+    public void setKmPrL() {this.kmPrL = kmPrL; }
+
+
+    public int getoktanTal() { return oktanTal; }
+    public int getKmPrL() {return kmPrL;}
 
     @Override
     public String toString() {
